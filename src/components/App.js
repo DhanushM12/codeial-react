0b6ec01b5ff597af519b856f2cf2121e788ae2f8
DhanushM12 <dhanushmukhi@gmail.com>
2020-07-04 19:48:19 +0530

    Added UI to Posts List

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/posts';
-
+import { PostsList } from './';
 //type on rcc to get the class
 class App extends React.Component {
   //type only cdm
@@ -10,8 +10,13 @@ class App extends React.Component {
   }
 
   render() {
-    console.log('PROPS', this.props);
-    return <div>App</div>;
+    const { posts } = this.props;
+    //console.log('PROPS', posts);
+    return (
+      <div>
+        <PostsList posts={posts} />
+      </div>
+    );
   }
 }
 
