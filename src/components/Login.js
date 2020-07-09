ac5e843da9ab7c28734fb99c5a12d5679835a7f8
DhanushM12 <dhanushmukhi@gmail.com>
2020-07-09 20:29:42 +0530

    Logging in user-3

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import login from '../actions/auth';
 
 class Login extends Component {
   constructor(props) {
@@ -29,11 +31,17 @@ class Login extends Component {
     //console.log('this.emailInputRef', this.emailInputRef);
     //console.log('this.passwordInputRef', this.passwordInputRef);
     console.log('this.state', this.state);
+    const { email, password } = this.state;
+    if (email && password) {
+      this.props.dispatch(login(email, password));
+    }
   };
   render() {
+    const { error, inProgress } = this.props.auth;
     return (
       <form className="login-form">
         <span className="login-signup-header">Log In</span>
+        {error && <div className="alert error-dailog">{error}</div>}
         <div className="field">
           <input
             type="email"
@@ -55,11 +63,25 @@ class Login extends Component {
           />
         </div>
         <div className="field">
-          <button onClick={this.handleSubmitForm}>Log In</button>
+          {inProgress ? (
+            <button onClick={this.handleSubmitForm} disabled={inProgress}>
+              Logging In...
+            </button>
+          ) : (
+            <button onClick={this.handleSubmitForm} disabled={inProgress}>
+              Log In
+            </button>
+          )}
         </div>
       </form>
     );
   }
 }
 
-export default Login;
+function mapStateToProps(state) {
+  return {
+    auth: state.auth,
+  };
+}
+
+export default connect(mapStateToProps)(Login);
