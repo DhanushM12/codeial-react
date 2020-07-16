c8ff6e69efa1f94e5e7da7b9b9408b4b380b8758
DhanushM12 <dhanushmukhi@gmail.com>
2020-07-17 01:32:42 +0530

    added and removed user as friend

--- src/components/UserPofile.js
+++ src/components/UserPofile.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchUserProfile } from '../actions/profile';
+import { APIUrls } from '../helpers/urls';
+import { getAuthTokenFromLocalStorage } from '../helpers/utils';
+import { addFriend, removeFriend } from '../actions/friends';
 
 class UserPofile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      success: null,
+      error: null,
+      successMessage: null,
+    };
+  }
   componentDidMount() {
     const { match } = this.props;
     if (match.params.userId) {
@@ -22,6 +33,61 @@ class UserPofile extends Component {
     return false;
   };
 
+  handleAddFriendClick = async () => {
+    const userId = this.props.match.params.userId;
+    const url = APIUrls.addFriend(userId);
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: `Bearer ${getAuthTokenFromLocalStorage()}`,
+      },
+    };
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    if (data.success) {
+      this.setState({
+        success: true,
+        successMessage: 'Added Friend Successfully!',
+      });
+      this.props.dispatch(addFriend(data.data.friendship));
+    } else {
+      this.setState({
+        success: null,
+        error: data.message,
+      });
+    }
+  };
+
+  handleRemoveFriendClick = async () => {
+    const { match } = this.props;
+    const url = APIUrls.removeFriend(match.params.userId);
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: `Bearer ${getAuthTokenFromLocalStorage()}`,
+      },
+    };
+    const response = await fetch(url, options);
+    const data = await response.json();
+    console.log('await data', data);
+
+    if (data.success) {
+      this.setState({
+        success: true,
+        successMessage: 'Removed Friend Successfully!',
+      });
+      this.props.dispatch(removeFriend(match.params.userId));
+    } else {
+      this.setState({
+        success: null,
+        error: data.message,
+      });
+    }
+  };
+
   render() {
     const {
       match: { params },
@@ -34,7 +100,7 @@ class UserPofile extends Component {
     }
 
     const isUserAFriend = this.checkIfUserIsAFriend();
-
+    const { success, error, successMessage } = this.state;
     return (
       <div className="settings">
         <div className="img-container">
@@ -53,10 +119,24 @@ class UserPofile extends Component {
         </div>
         <div className="btn-grp">
           {!isUserAFriend ? (
-            <button className="button save-btn">Add Friend</button>
+            <button
+              className="button save-btn"
+              onClick={this.handleAddFriendClick}
+            >
+              Add Friend
+            </button>
           ) : (
-            <button className="button save-btn">Remove Friend</button>
+            <button
+              className="button save-btn"
+              onClick={this.handleRemoveFriendClick}
+            >
+              Remove Friend
+            </button>
           )}
+          {success && (
+            <div className="alert success-dailog">{successMessage}</div>
+          )}
+          {error && <div className="alert error-dailog">{error}</div>}
         </div>
       </div>
     );
