df1bfa99e649ac17f948be0c1696cf5aafd034e1
DhanushM12 <dhanushmukhi@gmail.com>
2020-08-07 23:39:29 +0530

    added chat functionality using sockets

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import io from 'socket.io-client';
 import '../chat.css';
 class Chat extends Component {
   constructor(props) {
@@ -7,7 +9,50 @@ class Chat extends Component {
       messages: [], // {content: 'some message', self: true}
       typedMessage: '',
     };
+    this.socket = io.connect('http://54.237.158.65:5000');
+    this.userEmail = props.user.email;
+    console.log('PROPSSS', props);
+    if (this.userEmail) {
+      this.setupConnections();
+    }
   }
+  setupConnections = () => {
+    const socketConnection = this.socket;
+    const self = this;
+    this.socket.on('connect', function () {
+      console.log('connection extablished');
+      socketConnection.emit('join_room', {
+        user_email: this.userEmail,
+        chatroom: 'codeial',
+      });
+      socketConnection.on('user_joined', function (data) {
+        console.log('NEW USER Joined', data);
+      });
+    });
+    this.socket.on('receive_message', function (data) {
+      //add message to state
+      const { messages } = self.state;
+      const messageObject = {};
+      messageObject.content = data.message;
+      if (data.user_email === self.userEmail) {
+        messageObject.self = true;
+      }
+      self.setState({
+        messages: [...messages, messageObject],
+        typedMessage: '',
+      });
+    });
+  };
+  handleSubmit = () => {
+    const { typedMessage } = this.state;
+    if (typedMessage && this.userEmail) {
+      this.socket.emit('send_message', {
+        message: typedMessage,
+        user_email: this.userEmail,
+        chatroom: 'codeial',
+      });
+    }
+  };
   render() {
     const { typedMessage, messages } = this.state;
     return (
@@ -24,7 +69,7 @@ class Chat extends Component {
           {messages.map((message) => (
             <div
               className={
-                messages.self
+                message.self
                   ? 'chat-bubble self-chat'
                   : 'chat-bubble other-chat'
               }
@@ -46,4 +91,9 @@ class Chat extends Component {
   }
 }
 
-export default Chat;
+function mapStateToProps({ auth }) {
+  return {
+    user: auth.user,
+  };
+}
+export default connect(mapStateToProps)(Chat);
